7a427773e90b14760ffbc364c5cf027251b2f322
NauelG <g.nauel@gmail.com>
2017-09-16 18:27:56 +0200

    Modifico el color de la celda anterior para que el usuario conozca siempre su posicion

--- JS/autoplay.js
+++ JS/autoplay.js
@@ -5,10 +5,15 @@ var cronometer;
 
 var board = new Array(8);
 
+var CellSelected_x;
+var CellSelected_y;
+
 function SelectCell(x, y){
  board [x][y] = 1;
- PaintCell(x, y, "green");
+ PaintCell(CellSelected_x, CellSelected_y, "orange");
  PaintHorseCell(x, y, "green");
+ CellSelected_x = x;
+ CellSelected_y = y;
 }
 
 function CheckCell(x, y){
@@ -27,6 +32,9 @@ function autoplay(){
 
   x=Math.round(Math.random()*7);
   y=Math.round(Math.random()*7);
+
+  CellSelected_x = x;
+  CellSelected_y = y;
 
   SelectCell(x, y);
 }
