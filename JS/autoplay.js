5d4cd762327d598b4ab95801a0487410626a57df
NauelG <g.nauel@gmail.com>
2017-09-16 17:41:38 +0200

    Creo la funcion para identificar y administrar la matriz del tablero

--- JS/autoplay.js
+++ JS/autoplay.js
@@ -3,10 +3,14 @@ var total_secs;
 var total_mins;
 var cronometer;
 
+var board = new Array(8);
 
 
 function autoplay(){
   //alert("Hola!");
+
+  for (i=0; i<8; i++) board[i] = new Array(8);
+  
   ResetTime();
   startTime();
 }
